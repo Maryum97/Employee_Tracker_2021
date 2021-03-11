d14d5a1c32854100421cd23a5a283eee5b4fbf84
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-11 18:05:19 +0000

    updated sql

--- SQL_db's/seed.sql
+++ SQL_db's/seed.sql
@@ -1,10 +1,18 @@
 USE employee_db;
 
 INSERT INTO department (dep_name)
-VALUES ('Maintenance');
+VALUES ('Management'),
+('Operation'),
+('Maintenance'),
+('Assistance'),
+('Supervision');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Engineer', '100', '5');
+VALUES ('Manager', 3000, 1),
+('Engineer', 2500, 2),
+('Technician', 2000, 3),
+('Marketer', 3000, 4),
+('Supervisor', 3000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Maryum', 'Bokhari', '3', '10');
+VALUES ('Maryum', 'Bokhari', 2, 1);
