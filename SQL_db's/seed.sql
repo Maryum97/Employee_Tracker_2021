57d86333e4d0f32556689095721e24764c022573
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-11 18:20:33 +0000

    updated sql

--- SQL_db's/seed.sql
+++ SQL_db's/seed.sql
@@ -4,15 +4,15 @@ INSERT INTO department (dep_name)
 VALUES ('Management'),
 ('Operation'),
 ('Maintenance'),
-('Assistance'),
-('Supervision');
+('Sales'),
+('Legal');
 
 INSERT INTO role (title, salary, department_id)
 VALUES ('Manager', 3000, 1),
 ('Engineer', 2500, 2),
 ('Technician', 2000, 3),
 ('Marketer', 3000, 4),
-('Supervisor', 3000, 5);
+('Lawyer', 3000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Maryum', 'Bokhari', 2, 1);
