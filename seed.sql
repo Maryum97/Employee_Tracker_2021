978eb795f002834e3e27c3a076a0758f1d0d2ea0
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-11 13:24:42 +0000

    modified sql for JOIN

--- seed.sql
+++ seed.sql
@@ -1,10 +1,10 @@
 USE employee_db;
 
-INSERT INTO department (name)
-VALUES ('');
+INSERT INTO department (dep_name)
+VALUES ('Maintenance');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('', '', '');
+VALUES ('Cleaner', '100', '5');
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('', '', '', '');
+VALUES ('Maryum', 'Bokhari', '3', '10');
