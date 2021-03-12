8ef8b3593e8aa6b0cbe65b9c11f215ef1d71020b
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-03-12 21:18:36 +0000

    added values for MANAGER

--- SQL_db's/seed.sql
+++ SQL_db's/seed.sql
@@ -1,23 +1,26 @@
 USE employee_db;
 
 INSERT INTO department (dep_name)
-VALUES ('Management'),
+VALUES ('Finance'),
 ('Operation'),
 ('Maintenance'),
 ('Sales'),
 ('Legal');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Manager', 3000, 1),
-('Engineer', 2500, 2),
+VALUES ('Accountant', 3000, 1),
+('Engineer', 3000, 2),
 ('Technician', 2000, 3),
 ('Marketer', 3000, 4),
 ('Lawyer', 3000, 5);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Maryum', 'Bokhari', 1, null);
+VALUES (1, 'Maryum', 'Bokhari', 1, null),
+(2, 'Sarah', 'Amir', 1, 3),
+(3, 'Amjad', 'Shah', 4, 4);
 
 INSERT INTO manager (id, first_name, last_name)
 VALUES (1, 'Zahid', 'Bokhari'),
 (2, 'Betty', 'Smith'),
-(3, 'Martha', 'Mesa');
+(3, 'Martha', 'Mesa'),
+(4, 'Paul', 'Visak');
